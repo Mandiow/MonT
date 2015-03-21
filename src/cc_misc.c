4964f8deb1de70d08a28e37460d51b91df1ab806
Caiã Aragão <cafrazao@inf.ufrgs.br>
2015-03-21 00:43:18 -0300

    Update cc_misc.c

--- src/cc_misc.c
+++ src/cc_misc.c
@@ -1,12 +1,12 @@
 #include "cc_misc.h"
-
-
+#include "cc_dict.h"
 
 extern int lineNumber = 1;
+
 int getLineNumber (void)
 {
-  lineNumber++;
-  return -1;
+  //++lineNumber;
+  return lineNumber;
 }
 
 void yyerror (char const *mensagem)
@@ -17,9 +17,11 @@ void yyerror (char const *mensagem)
 void main_init (int argc, char **argv)
 {
   //implemente esta função com rotinas de inicialização, se necessário
+	hashtable = dict_create(100);
 }
 
 void main_finalize (void)
 {
+  explode_tudo();
   //implemente esta função com rotinas de inicialização, se necessário
 }
