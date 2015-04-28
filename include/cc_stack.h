9bc94acf88626c64fa534914d6e7cb61f81a7439
agwermann <alexandrewermann@gmail.com>
2015-04-28 10:17:54 -0300

    criando lista de identificadores para a pilha para o escopo aninhado

--- include/cc_stack.h
+++ include/cc_stack.h
@@ -4,17 +4,34 @@
 #include "cc_ast.h"
 #include "cc_dict.h"
 
+
+
 typedef struct cel { 
 	int conteudo; 
 	struct cel *prox; 
-} celula;
+} PilhaIdentificadores;
+
+typedef struct ListaId { 
+	int identificador; 
+	struct Lista *prox; 
+} ListaIdentificadores;
 
 
 
-void inicia(cedula *pilha);
+
+void CriaPilha(PilhaIdentificadores *pilha);
 void empilha(int y, celula *tp);
-int desempilha(celula *tp);
+int desempilha(PilhaIdentificadores *tp);
 void destroi(cedula *pilha);
+
+
+// operações com a lista de identificadores
+void CriaListaIdentificadores(ListaIdentificadores *listaId);
+void AdicionaListaIdentificadores(int valor, ListaIdentificadores *listaId);
+void RemoveElementoListaIdentificadoresInicio(ListaIdentificadores *listaId);
+void RemoveElementoListaIdentificadoresConteudo(int valor, ListaIdentificadores *listaId);
+void destroiListaIdentificadores(ListaIdentificadores *listaId);
+
 
 #endif
 
