2a29193674f5ef546b0e5a91738736c8f48c4cbf
Inatan <inatan.hertzog@inf.ufrgs.br>
2015-05-12 15:42:33 -0300

    Atualização lista de instruções.
    Manter tudo de acordo com o enunciado.

--- include/cc_list.h
+++ include/cc_list.h
@@ -2,6 +2,7 @@
 #define __CC_LIST_H__
 
 #include "cc_iloc.h"
+
 /*
 * ILOC_operand
 * um operando pode ser um literal, um registrado ou um valor da memória
@@ -9,8 +10,8 @@
 */
 union ILOC_operand {
 	int val;
-	ILOC_register_t reg;
-	ILOC_memory_cell_t mem;
+	ILOC_register_t r;
+	ILOC_label_t L;
 }
 
 /*
@@ -23,22 +24,13 @@ typedef struct ILOC_operand_list{
 }ILOC_operand_list_t;
 
 /*
-* ILOC_operand_target
-* Operando o qual vão receber resultados das instruções só podem ser valores de memória e registradores
-*/
-union ILOC_operand_target{
-	ILOC_register_t reg;
-	ILOC_memory_cell_t mem;
-}
-
-/*
 * ILOC_instruction_t
 * Uma instrução possui uma operação, uma lista de operandos fontes e um operando alvo para o resultado da operação
 */
 typedef struct ILOC_instruction{
 	ILOC_op_t operation;
-	ILOC_operand_list *operand_font;
-	ILOC_operand_target op_target;
+	ILOC_operand_list_t *operand_font;
+	ILOC_operand_list_t *operand_target;
 } ILOC_instruction_t;
 
 /*
@@ -47,7 +39,7 @@ typedef struct ILOC_instruction{
 */
 typedef struct ILOC_instruction_list{
 	ILOC_instrution instruction;
-	struct ILOC_instruction_list * prox;
+	struct ILOC_instruction_list *prox;
 	ILOC_label_t label;
 }ILOC_instruction_list_t;
 
